2cda62fc9e341e2b42e7630d1f6a05f3e614062e
Devi Acharya <dacharya64@gmail.com>
2019-03-05 11:39:55 -0800

    removing old code

--- CardCropper.py
+++ CardCropper.py
@@ -4,24 +4,7 @@ import os
 from PIL import Image
 
 image_list = []
-for filename in glob.glob('cards/*.png'): #assuming gif
-    # print(filename)
+for filename in glob.glob('cards/*.png'): 
     im=Image.open(filename)
     im = im.crop((30, 40, 200, 170))
     im.save(filename + '_c.png')
-
-    #image_list.append(im)
-
-
-        # try:
-        #
-        #     im = Image.open(f).convert('L')
-        #     im = im.crop((1, 1, 98, 33))
-        #     im.save('_0.png')
-        # except (KeyboardInterrupt, SystemExit):
-        #     raise
-        # except Exception as e:
-        #     print("Ignoring problem file:", f)
-        #     print(str(e))
-        #     print(repr(e))
-        #     continue
