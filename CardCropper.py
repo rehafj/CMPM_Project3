b8203674931e5a60eb02e65df42dfceb250af1ef
Devi Acharya <dacharya64@gmail.com>
2019-03-05 11:51:24 -0800

    cropper works better

--- CardCropper.py
+++ CardCropper.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 
 image_list = []
-for filename in glob.glob('cards/*.png'): 
-    im=Image.open(filename)
-    im = im.crop((30, 40, 200, 170))
-    im.save(filename + '_c.png')
+for filename in glob.glob('cards/*.png'):
+    im = Image.open(filename)
+    im = im.crop((30, 40, 220, 170))
+    im.save(filename)
