9377b00a85f5346638ab34b47ac8a2bb747866b0
mirekSBCS <33669390+mirekSBCS@users.noreply.github.com>
2019-03-06 11:09:38 -0800

    Sorted Into Train/Test/Val

--- CardSorter2.py
+++ CardSorter2.py
@@ -1,41 +1,18 @@
 import os
 import glob
 import shutil
+import random
 
-from PIL import Image
-from pokemontcgsdk import Card
-from pokemontcgsdk import Set
-from pokemontcgsdk import Type
-from pokemontcgsdk import Supertype
-from pokemontcgsdk import Subtype
-
-for filename in glob.glob('cards/*.png'):
+for filename in glob.glob('Water/*.png'):
+    print(filename)
     name_ext = filename.split("\\")[1]
     # print(name_ext)
     name = name_ext.split(".")[0]
-    name_dir = "./cards/" + name_ext
-    card = Card.find(name)
-    print(name_dir)
-    card_type = card.types[0]
-    if (card_type == "Colorless"):
-        shutil.move(name_dir, "./cards/Colorless/" + name_ext)
-    elif (card_type == "Darkness"):
-        shutil.move(name_dir, "./cards/Darkness/" + name_ext)
-    elif (card_type == "Dragon"):
-        shutil.move(name_dir, "./cards/Dragon/" + name_ext)
-    elif (card_type == "Fairy"):
-        shutil.move(name_dir, "./cards/Fairy/" + name_ext)
-    elif (card_type == "Fighting"):
-        shutil.move(name_dir, "./cards/Fighting/" + name_ext)
-    elif (card_type == "Fire"):
-        shutil.move(name_dir, "./cards/Fire/" + name_ext)
-    elif (card_type == "Grass"):
-        shutil.move(name_dir, "./cards/Grass/" + name_ext)
-    elif (card_type == "Lightning"):
-        shutil.move(name_dir, "./cards/Lightning/" + name_ext)
-    elif (card_type == "Metal"):
-        shutil.move(name_dir, "./cards/Metal/" + name_ext)
-    elif (card_type == "Psychic"):
-        shutil.move(name_dir, "./cards/Psychic/" + name_ext)
+    name_dir = "Water/" + name_ext
+    rand = random.randint(1, 100)
+    if (rand > 80):
+        shutil.move(name_dir, "./Test/Water/" + name_ext)
+    elif (rand > 16):
+        shutil.move(name_dir, "./Train/Water/" + name_ext)
     else:
-        shutil.move(name_dir, "./cards/Water/" + name_ext)
+        shutil.move(name_dir, "./Val/Water/" + name_ext)
