cf71990e307c8ff9a38c7df3df8b24cb02e735dc
Devi Acharya <dacharya64@gmail.com>
2019-03-07 09:52:23 -0800

    fixed the attack scraper

--- AttackReader.py
+++ AttackReader.py
@@ -15,96 +15,96 @@ for card in cards:
             for attack in card.attacks:
                 f = open("colorless_attack.txt", "a")
                 f.write(card_type + ",")
-                f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Darkness"):
             for attack in card.attacks:
                 f = open("psychic_attack.txt", "a")
                 f.write("Psychic,")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Dragon"):
             for attack in card.attacks:
                 f = open("water_attack.txt", "a")
                 f.write("Water,")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Fairy"):
             for attack in card.attacks:
                 f = open("colorless_attack.txt", "a")
                 f.write("Colorless,")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Fighting"):
             for attack in card.attacks:
                 f = open("fighting_attack.txt", "a")
                 f.write(card_type + ",")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Fire"):
             for attack in card.attacks:
                 f = open("fire_attack.txt", "a")
                 f.write(card_type + ",")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Grass"):
             for attack in card.attacks:
                 f = open("grass_attack.txt", "a")
                 f.write(card_type + ",")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Lightning"):
             for attack in card.attacks:
                 f = open("lightning_attack.txt", "a")
                 f.write(card_type + ",")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Metal"):
             for attack in card.attacks:
@@ -116,28 +116,28 @@ for card in cards:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         elif (card_type == "Psychic"):
             for attack in card.attacks:
                 f = open("psychic_attack.txt", "a")
                 f.write(card_type + ",")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
         else: # water
             for attack in card.attacks:
                 f = open("water_attack.txt", "a")
                 if "cost" in attack:
-                    f.write('\"' + json.dumps(attack["cost"]) + '\",')
+                    f.write('\"' + json.dumps(attack["cost"]).replace('\"', '\'') + '\",')
                 if "text" in attack:
                     f.write(json.dumps(attack["text"]) +  ',')
                 if "damage" in attack:
                     f.write(json.dumps(attack["damage"]))
-                f.write("\n\n")
+                f.write("\n")
                 f.close()
