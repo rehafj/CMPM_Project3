452a3b07fe39f1191f234172179c75e87eda7c20
rj-90 <rj.codesstuff@gmail.com>
2019-03-08 19:19:46 -0800

    Update pokeGAN.py
    1x1 print images instead of 8x8
    added in our images

--- Github_pokrGan_ Siraj /pokeGAN.py
+++ Github_pokrGan_ Siraj /pokeGAN.py
@@ -247,7 +247,8 @@ def train():
             imgtest = sess.run(fake_image, feed_dict={random_input: sample_noise, is_train: False})
             # imgtest = imgtest * 255.0
             # imgtest.astype(np.uint8)
-            save_images(imgtest, [8,8] ,newPoke_path + '/epoch' + str(i) + '.jpg')
+            save_images(imgtest, [1,1] ,newPoke_path + '/epoch' + str(i) + '.jpg')
+
 
             print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
     coord.request_stop()
