53d76246d447351bd23248b8001c980780db52e3
JazzTap <ottojasmine@gmail.com>
2019-03-08 16:50:10 -0800

    Altered Manu's Code With other GAN examples

--- manu_code_GAN/utils.py
+++ manu_code_GAN/utils.py
@@ -10,8 +10,8 @@ N_CHANNEL = 3
 LEARNING_RATE = 0.0001
 BATCH_SIZE = 5
 N_ITERATION = 150000
-IMAGE_WIDTH = 64
-IMAGE_HEIGHT = 64
+IMAGE_WIDTH = 190
+IMAGE_HEIGHT = 110
 IMAGE_EXT ='png'
 
 DATASET_PATH ='dataset/'
@@ -25,13 +25,13 @@ def initialize(sess):
 
     if not os.path.exists(CKPT_DIR):
         os.makedirs(CKPT_DIR)
-    
+
     ckpt = tf.train.get_checkpoint_state(os.path.dirname(CKPT_DIR))
     if ckpt and ckpt.model_checkpoint_path:
         saver.restore(sess, ckpt.model_checkpoint_path)
 
     return saver
-    
+
 def shuffle_in_unison(a, b):
      n_elem = a.shape[0]
      indices = np.random.permutation(n_elem)
@@ -48,7 +48,7 @@ def load_data(dataset_path):
     image_array=[]
 
     file_list = glob.glob(dataset_path + '/*.'+IMAGE_EXT)
-  
+
     temp_image_array = np.array([np.array(scipy.misc.imresize(scipy.misc.imread(file_name, mode='RGB').astype('float32'),(IMAGE_HEIGHT, IMAGE_WIDTH))) for file_name in file_list])
     temp_image_array = temp_image_array/127. -1
 
@@ -56,5 +56,3 @@ def load_data(dataset_path):
     image_array = np.concatenate(image_array, axis=0)
 
     return image_array
-
-
