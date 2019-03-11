225dfe9bd2c8a2f5eeda0e52905ba5c527949f3e
Devi Acharya <dacharya64@gmail.com>
2019-03-11 15:20:01 -0700

    names and json corrected

--- NameReader2.py
+++ NameReader2.py
@@ -10,10 +10,23 @@ from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 
+def setCardType(type):
+    if (type == "Darkness"):
+        return "Psychic"
+    elif (type == "Dragon"):
+        return "Water"
+    elif (type == "Fairy"):
+        return "Colorless"
+    elif (type == "Metal"):
+        return "Fighting"
+    else:
+        return type
+
 cards = Card.where(supertype='pokemon')
 f = open("names.txt", "a", encoding="utf-8")
 for card in cards:
-    f.write(str(card.types[0]) + ", ")
+    type = setCardType(card.types[0])
+    f.write(str(type) + ", ")
     f.write(str(card.name))
     f.write("\n")
 f.close()
